10fc2a15fac5eb2590f634f3455c4d89bc83f63d
C778702 <raffael.zingg@axa-winterthur.ch>
2023-06-15 13:09:08 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+  /*
   cloud {
-    organization = "organization-name"
+    organization = "sandbox-environment"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+  */
 
   required_providers {
     aws = {
